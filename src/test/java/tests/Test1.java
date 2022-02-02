8fdec14ac37d6f96c04e746bf119370f265bf04e
mervealgi <72434705+mervealgi@users.noreply.github.com>
2022-02-02 23:49:10 +0300

    Initial commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+        driver.get("https://www.imdb.com/");
+    }
+
+
+}
