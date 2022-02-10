316e18127b0e5055020030927cd5828e97411344
mervealgi <72434705+mervealgi@users.noreply.github.com>
2022-02-10 23:03:17 +0300

    11

--- src/test/java/tests/downloadFile.java
+++ src/test/java/tests/downloadFile.java
@@ -1,7 +1,11 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
 
 public class downloadFile {
 
@@ -12,5 +16,26 @@ public class downloadFile {
 
         driver.get("https://demoqa.com/upload-download");
         driver.manage().window().maximize();
+
+        WebElement downloadButton = driver.findElement(By.id("downloadButton"));
+        downloadButton.click();
+
+        String downloadPath = "Users/Dell/Downloads";
+        String downloadFileName = "sampleFile.jpg";
+
+        boolean isDownloaded = isFileDownloaded(downloadPath,downloadFileName);
+        System.out.println(isDownloaded);
+    }
+
+    public static boolean isFileDownloaded(String path, String fileName){
+
+        File file = new File(path);
+        File[] files = file.listFiles();
+
+        for(int i = 0; i <files.length; i++){
+            if(files[i].getName().equals(fileName)){
+                return true;
+            }
+        }return false;
     }
 }
