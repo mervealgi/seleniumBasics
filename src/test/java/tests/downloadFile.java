0bb51b7f354f95d7ba72c51dd4f9fa6553de410a
mervealgi <72434705+mervealgi@users.noreply.github.com>
2022-02-11 16:56:30 +0300

    12

--- src/test/java/tests/downloadFile.java
+++ src/test/java/tests/downloadFile.java
@@ -9,7 +9,7 @@ import java.io.File;
 
 public class downloadFile {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -20,8 +20,9 @@ public class downloadFile {
         WebElement downloadButton = driver.findElement(By.id("downloadButton"));
         downloadButton.click();
 
-        String downloadPath = "/Users//Dell//Downloads";
+        String downloadPath = "C:\\Users\\Dell\\Downloads";   //your path
         String downloadFileName = "sampleFile.jpg";
+        Thread.sleep(5000);
 
         boolean isDownloaded = isFileDownloaded(downloadPath,downloadFileName);
         System.out.println(isDownloaded);
@@ -34,6 +35,7 @@ public class downloadFile {
 
         for(int i = 0; i <files.length; i++){
             if(files[i].getName().equals(fileName)){
+                files[i].delete();
                 return true;
             }
         }return false;
