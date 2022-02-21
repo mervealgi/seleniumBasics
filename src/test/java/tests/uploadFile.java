d4a9c2427f59e9932f4eb559f6e09a305ff2e181
mervealgi <72434705+mervealgi@users.noreply.github.com>
2022-02-21 21:01:20 +0300

    21.02

--- src/test/java/tests/uploadFile.java
+++ src/test/java/tests/uploadFile.java
@@ -1,4 +1,23 @@
 package tests;
 
+import net.jodah.failsafe.internal.util.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class uploadFile {
+
+    public static void main(String[] args){
+
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demoqa.com/upload-download");
+        driver.manage().window().maximize();
+
+        WebElement uplButton = driver.findElement(By.id("uploadFile"));
+        uplButton.sendKeys("C:\\Users\\Dell\\Desktop\\Antonio Tabucchi - Ufuk Çizgisi.pdf");
+        String ActualTitle = driver.getTitle();
+    }
 }
